7cc5124b79a5564cc7c3591238e21fc6e2d69f38
Foxglovery <foxglovery@gmail.com>
2024-12-29 20:45:51 -0600

    variables

--- test.php
+++ test.php
@@ -1,3 +1,7 @@
+<h2>I sure do hope I learn how to P the HP</h2>
 <?php
-echo "<h1> My Name is Gabriel </h1>";
+$numberClicked = 0;
+$didItClickEvenly = false;
+
+echo "<h1> My sssss is Gabriel </h1>";
 ?>
